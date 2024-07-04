8bc0118c47887ade89b8742397bcd6083592027b
Max <max08122000@gmail.com>
2024-07-05 01:43:01 +0200

    V0.0.9 Code clear, bugfixes and improvements. Filter works well.

--- YoutubeAnalyser.py
+++ YoutubeAnalyser.py
@@ -9,7 +9,7 @@ import os
 BASE_URL = 'https://youtube138.p.rapidapi.com/video/details/'
 settings = {}
 # Auslesen funktionen
-# Standard-Konfigurationsdatei
+# Standard Konfigurationsdatei
 def create_default_config(config_file):
     default_config = {
         "Downloadart": "zip",
@@ -17,15 +17,20 @@ def create_default_config(config_file):
         "API Key": "",
         "Youtube Links": [],
         "Downloadpfad": "",
-        "Zahl": 5,
-        "Boolean": true
+        "Filter": {
+            "title": True,
+            "publishedDate": True,
+            "viewCount": True,
+            "likeCount": True,
+            "commentCount": True
+        }
     }
     with open(config_file, 'w') as file:
         json.dump(default_config, file, indent=4)
-    print(f"Standard-Konfigurationsdatei {config_file} wurde erstellt.")
+    print(f"Standard Konfigurationsdatei {config_file} wurde erstellt.")
 
 
-#Einlesen der Konfigurationsdatei und Validierung der Einstellungen
+#Einlesen der Konfigurationsdatei und Überprüfung der Einstellungen
 def load_config(config_file):
     if not os.path.exists(config_file):
         create_default_config(config_file)
@@ -36,7 +41,7 @@ def load_config(config_file):
 
 
 
-    # Downloadart validieren
+    # Downloadart
     valid_download_types = ['tar', 'zip', 'txt']
     download_type = config.get('Downloadart')
     if download_type in valid_download_types:
@@ -53,48 +58,40 @@ def load_config(config_file):
     else:
         raise ValueError("Fehlerhafte Configfile: Modus ist ungültig.")
 
-    # API Key validieren
+    # API Key
     api_key = config.get('API Key')
     if api_key:
         settings['API Key'] = api_key
     else:
         raise ValueError("Fehlerhafte Configfile: API Key darf nicht leer sein.")
 
-    # YouTube Links validieren
+    # YouTube Links
     youtube_links = config.get('Youtube Links')
-    for ylink in youtube_links:
-        ylink.replace("https://www.youtube.com/watch?v=", "")
-
     if youtube_links and isinstance(youtube_links, list) and all(isinstance(link, str) for link in youtube_links):
-        settings['Youtube Links'] = youtube_links
-
+        settings['Youtube Links'] = [link.replace("https://www.youtube.com/watch?v=", "") for link in youtube_links]
     else:
-        raise ValueError("Fehlerhafte Configfile: Youtube Links darf nicht leer sein und muss eine Liste von Strings sein.")
+        raise ValueError("Fehlerhafte Configfile: Youtube Links darf nicht leer sein.")
 
-    # Downloadpfad validieren
+    # Downloadpfad
     download_path = config.get('Downloadpfad', '')
     if not download_path:
         download_path = os.getcwd()
     settings['Downloadpfad'] = download_path
 
-    # Zahl von 1-10 einlesen
-    number = config.get('Zahl')
-    if isinstance(number, int) and 1 <= number <= 10:
-        settings['Zahl'] = number
-    else:
-        raise ValueError("Fehlerhafte Configfile: Zahl muss eine Zahl zwischen 1 und 10 sein.")
 
-    # Boolean einlesen
-    boolean_value = config.get('Boolean')
-    if isinstance(boolean_value, bool):
-        settings['Boolean'] = boolean_value
-    else:
-        raise ValueError("Fehlerhafte Configfile: Boolean muss ein boolescher Wert sein.")
-
+    # Filter einlesen
+    filter_settings = config.get('Filter', {})
+    settings['Filter'] = {
+        'title': filter_settings.get('title', True),
+        'publishedDate': filter_settings.get('publishedDate', True),
+        'viewCount': filter_settings.get('viewCount', True),
+        'likeCount': filter_settings.get('likeCount', True),
+        'commentCount': filter_settings.get('commentCount', True)
+    }
     return settings
 
-# API
-# Funktion zum Abrufen von Videodetails
+#API
+#Funktion zum Abrufen von Videodetails
 def get_video_details(video_id):
 
     headers = {
@@ -120,38 +117,27 @@ def get_video_details(video_id):
         return None
 
 
-# compare and filter
-# Funktion zum Vergleichen und Sortieren der Arrays
-def compare_and_sort(arrays, output_count):
-    comparisons = []
+#compare and filter
+#Funktion zum Vergleichen und Sortieren der Arrays
+def compare_and_sort(data):
+    compare = pd.concat(data)
+    compare2 = compare.max()
+    return compare2.to_dict()
 
-    # Vergleich der Arrays (nur die ersten drei Arrays werden verglichen)
-    for i in range(len(arrays[0])):
-        max_value = max(arrays[0][i], arrays[1][i], arrays[2][i])
-        comparisons.append(max_value)
-
-    # Absteigend sortieren
-    comparisons.sort(reverse=True)
-
-    # Begrenzen der Ausgabe auf die gewünschten Anzahl der größten Zahlen
-    return comparisons[:output_count]
-
-
-# Funktion zum Schreiben der Arrays in eine .txt-Datei
-def write_arrays_to_file(arrays, filename):
+#Funktion zum Schreiben der Arrays in eine .txt
+def write_arrays_to_file(data, filename):
     with open(filename, 'w') as file:
-        for array in arrays:
-            file.write(' '.join(map(str, array)) + '\n\n')
+        for key, value in data.items():
+            file.write(f"{key}: {value}\n")
     print(f"Arrays wurden in '{filename}' geschrieben.")
 
-
-# Funktion zum Filtern und Schreiben der Arrays basierend auf Booleans
-def filter_and_write_arrays(arrays, booleans, filename):
+#Funktion zum Filtern
+def filter_and_write_data(data, filter_settings, filename):
     with open(filename, 'w') as file:
-        for array, boolean in zip(arrays, booleans):
-            if boolean:
-                file.write(' '.join(map(str, array)) + '\n\n')
-    print(f"Gefilterte Arrays wurden in '{filename}' geschrieben.")
+        for key, value in data.items():
+            if filter_settings.get(key, True):
+                file.write(f"{key}: {value}\n")
+    print(f"Gefilterte Daten wurden in '{filename}' geschrieben.")
 
 
 
@@ -172,22 +158,42 @@ def filter_and_write_arrays(arrays, booleans, filename):
 
 
 
-# Main
+#Main
 if __name__ == "__main__":
-    config_file_path = 'config.json'
-    try:
-        settings = load_config(config_file_path)
-        print("Einstellungen erfolgreich geladen:", settings)
-        videolist = settings['Youtube Links']
-        video_id = videolist[0]
-        video_details = get_video_details(video_id)
-        if video_details:
-            df = pd.DataFrame([video_details])
-            print(df)
-            df.to_csv('video_details.csv', index=False)
 
-        #for arrays in settings['Youtube Links']:
-        #   array1 = video_info
+    config_path = 'config.json'
+    try:
+        settings = load_config(config_path)
+        print("Einstellungen erfolgreich geladen:", settings)
+
+        all_video_details = []
+
+        for video_id in settings['Youtube Links']:
+            video_details = get_video_details(video_id)
+            if video_details:
+                all_video_details.append(video_details)
+
+        if not all_video_details:
+            raise ValueError("Es gibt keine Videodetails.")
+
+        csv = pd.DataFrame(all_video_details)
+        csv.to_csv(os.path.join(settings['Downloadpfad'], 'output.csv'), index=False)
+
+        if settings['Modus'] == "Compare":
+            result = compare_and_sort([csv])
+            write_arrays_to_file(result, 'output.txt')
+        elif settings['Modus'] == "None":
+            write_arrays_to_file(all_video_details, 'output.txt')
+
+        elif settings['Modus'] == "Filter":
+            for video in all_video_details:
+                filter_and_write_data(video, settings['Filter'], 'output.txt')
+        elif settings['Modus'] == "Both":
+            for video in all_video_details:
+                filter_and_write_data(video, settings['Filter'], 'output.txt')
+
+        else:
+            print(f"Der Modus {settings['Modus']} wird derzeit nicht unterstützt.")
 
 
 
@@ -197,41 +203,8 @@ if __name__ == "__main__":
         #for link in settings['Youtube Links']:
         #   arrays = arrays.append(get_video_details(link))
         #array1 = get_video_details()
-        array1 = [15, 25, 35]
-        array2 = [15, 25, 35]
-        array3 = [5, 50, 40]
-        array4 = [10, 20, 30]
-        array5 = [10, 20, 30]
-
-        # Booleans für den Filtermodus
-        a = True
-        b = False
-        c = True
-        d = True
-        e = False
-
-        # Variable für die Anzahl der auszugebenden größten Zahlen
-        output_count = 2
 
 
-
-        # Hauptlogik basierend auf dem Modus
-        arrays = [array1, array2, array3, array4, array5]
-        booleans = [a, b, c, d, e]
-
-        if settings['Modus'] == "Compare":
-            result = compare_and_sort(arrays, output_count)
-            write_arrays_to_file(arrays, 'output_compare.txt')
-        elif settings['Modus'] == "None":
-            write_arrays_to_file(arrays, 'output_none.txt')
-        elif settings['Modus'] == "Filter":
-            filter_and_write_arrays(arrays, booleans, 'output_filter.txt')
-        elif settings['Modus'] == "Both":
-            result = compare_and_sort(arrays, output_count)
-            print("Ergebnis der Vergleiche:", result)
-            filter_and_write_arrays(arrays, booleans, 'output_both.txt')
-        else:
-            print(f"Der Modus {settings['Modus']} wird derzeit nicht unterstützt.")
 
 
     except Exception as e:
